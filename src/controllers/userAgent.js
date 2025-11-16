174f5033b2c05bc0dd7b80085bc75943cb74d25e
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-11-16 23:58:47 +0600

    send json data in a formatted way

--- src/controllers/userAgent.js
+++ src/controllers/userAgent.js
@@ -1,12 +1,24 @@
+const fs = require("fs")
+const path = require("path")
+const userAgentFile = path.join(__dirname, "../userAgents.json")
+
 const getUserAgents = (req, res) => {
-  res.status(200).json({
-    Postman: 2,
-    "Thunder Client": 2,
-    "Chrome Browser": 2,
-    "Edge Browser": 2,
-    curl: 2,
-    Other: 0,
-  })
+  const rowFile = fs.readFileSync(userAgentFile)
+  const userAgents = rowFile ? JSON.parse(rowFile) : []
+  const formatedData = userAgents.reduce((acc, item) => {
+    acc[item] = (acc[item] || 0) + 1
+    return acc
+  }, {})
+
+  res.status(200).send(JSON.stringify(formatedData, null, 2))
+  // res.status(200).json({
+  //   Postman: 2,
+  //   "Thunder Client": 2,
+  //   "Chrome Browser": 2,
+  //   "Edge Browser": 2,
+  //   curl: 2,
+  //   Other: 0,
+  // })
 }
 
 module.exports = { getUserAgents }
