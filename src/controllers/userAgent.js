f86fa00ab073a75ab4aaf52addc59a7e2bd36d60
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-11-17 01:47:05 +0600

    organize the response with frequency counter in json format

--- src/controllers/userAgent.js
+++ src/controllers/userAgent.js
@@ -1,16 +1,30 @@
-const fs = require("fs")
-const path = require("path")
-const userAgentFile = path.join(__dirname, "../userAgents.json")
+const fs = require("fs");
+const path = require("path");
+const userAgentFile = path.join(__dirname, "../userAgents.json");
 
 const getUserAgents = (req, res) => {
-  const rowFile = fs.readFileSync(userAgentFile)
-  const userAgents = rowFile ? JSON.parse(rowFile) : []
+  const rowFile = fs.readFileSync(userAgentFile);
+  const userAgents = rowFile ? JSON.parse(rowFile) : [];
+
+  const initialData = {
+    Postman: 0,
+    "Thunder Client": 0,
+    "Chrome Browser": 0,
+    "Edge Browser": 0,
+    curl: 0,
+    Other: 0,
+  };
   const formatedData = userAgents.reduce((acc, item) => {
-    acc[item] = (acc[item] || 0) + 1
-    return acc
-  }, {})
+    if (item.includes("Postman")) acc["Postman"] += 1;
+    else if (item.includes("Thunder Client")) acc["Thunder Client"] += 1;
+    else if (item.includes("Edg")) acc["Edge Browser"] += 1;
+    else if (item.includes("Chrome")) acc["Chrome Browser"] += 1;
+    else if (item.includes("curl")) acc["curl"] += 1;
+    else acc["Other"] += 1;
+    return acc;
+  }, initialData);
 
-  res.status(200).send(JSON.stringify(formatedData, null, 2))
-}
+  res.status(200).send(JSON.stringify(formatedData, null, 2));
+};
 
-module.exports = { getUserAgents }
+module.exports = { getUserAgents };
